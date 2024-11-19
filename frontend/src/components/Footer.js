be32d17f4a9679980f16e390772e3ae45888e65d
fentahunM <fantahun360@gmail.com>
2024-11-19 12:12:37 +0300

    initial commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+export default function Footer() {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+        </Row>
+      </Container>
+      ;
+    </footer>
+  );
+}
