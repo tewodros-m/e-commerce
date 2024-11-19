46c9fe96ddc52cdc7423f00ceeaf818977c806fb
fentahunM <fantahun360@gmail.com>
2024-11-19 23:58:00 +0300

    build sample views and urls

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .products import products
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    return Response('Hello')
+
+@api_view(['GET'])
+def getProducts(reauest):
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+    return Response(product)
