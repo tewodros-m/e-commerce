627a7d61a203546fc6322f31d6e5a3d14b2f664f
fentahunM <fantahun360@gmail.com>
2024-11-21 22:45:36 +0300

    build product detail reducer and actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducers } from './reducers/productReducers';
+import {
+  productDetailReducers,
+  productListReducers,
+} from './reducers/productReducers';
 
 const reducer = combineReducers({
   productList: productListReducers,
+  productDetails: productDetailReducers,
 });
 
 const initialState = {};
