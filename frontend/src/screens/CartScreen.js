7f92f4a02b3f91bc78fa721a6c1cd6cdb7bd4fd6
fentahunM <fantahun360@gmail.com>
2024-11-23 20:46:07 +0300

    add to cart functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useSearchParams } from 'react-router-dom';
+import { CART_ADD_ITEM } from '../constants/cartConstants';
+import { addToCart } from '../actions/cartActions';
 
 function CartScreen() {
+  const { id: productId } = useParams();
+  const [searchParams] = useSearchParams();
+  const qty = searchParams.get('qty') ? Number(searchParams.get('qty')) : 1;
+
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+  console.log(cartItems);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [productId, qty, dispatch]);
+
   return <div>CartScreen</div>;
 }
 
