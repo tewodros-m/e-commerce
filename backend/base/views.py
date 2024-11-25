bcad121c83e8e1011148f974069d410adf8426d7
fentahunM <fantahun360@gmail.com>
2024-11-25 16:47:44 +0300

    customize jwt

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,11 +1,36 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from .products import products
 from .models import Product
 from .serializers import ProductSerializer
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    # @classmethod
+    # def get_token(cls, user):
+    #     token = super().get_token(user)
+
+    #     # Add custom claims
+    #     token['username'] = user.username
+    #     token['message'] = 'hello world'
+
+    #     return token
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
