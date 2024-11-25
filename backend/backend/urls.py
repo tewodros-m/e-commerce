20cb0be8ea673491e7d05b49430d30eba14de187
fentahunM <fantahun360@gmail.com>
2024-11-25 16:07:00 +0300

    install and set up jwt

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,8 +19,12 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+# )
 
 urlpatterns = [
+    # path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('admin/', admin.site.urls),
     path('api/',include('base.urls'))
 ]
