3b9b4eb8261dc8d99ccf4866f18505489ace918e
fentahunM <fantahun360@gmail.com>
2024-11-25 18:54:55 +0300

    protect user routes

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,10 +2,12 @@ from typing import Any, Dict
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.models import User
 
 from .products import products
 from .models import Product
@@ -42,9 +44,17 @@ def getRoutes(request):
     return Response('Hello')
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
