73a47fc740c2ab55f06d51a553ee4b640f8414b1
fentahunM <fantahun360@gmail.com>
2024-11-27 16:00:10 +0300

    update profile endpoint

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -8,6 +8,7 @@ from base.views import user_views as views
 urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('profile/', views.getUserProfile, name='users-profile'),
+    path('profile/update', views.updateUserProfile, name='users-profile-update'),
     path('register/', views.registerUser, name='register'),
     path('', views.getUsers, name='users'),
 ]
