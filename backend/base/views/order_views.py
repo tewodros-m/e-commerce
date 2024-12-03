1912ab1cdab2cd066966081ab68c7db6d930e178
fentahunM <fantahun360@gmail.com>
2024-12-03 18:56:32 +0300

    get my orders endpoint

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -60,6 +60,17 @@ def addOrderItems(request):
         serializer = OrderSerializer(order, many=False)
         return Response(serializer.data)
     
+
+@api_view(['GET'])
+@permission_classes(IsAuthenticated)
+def getMyOrders(request):
+    user = request.user
+    print('USER', user)
+    orders = user.order_set.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data);
+
+    
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getOrderById(request, pk):
