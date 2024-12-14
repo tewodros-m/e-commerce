3ad3327eff9b185752c8b3af4b0a690cd01d946f
fentahunM <fantahun360@gmail.com>
2024-12-14 21:57:33 +0300

    build get user and update user endpoint

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('register/', views.registerUser, name='register'),
     path('', views.getUsers, name='users'),
 
+    path('<str:pk>/', views.getUserById, name='user'),
+    path('update/<str:pk>/', views.updateUser, name='user-update'),
+
     path('delete/<str:pk>/', views.deleteUser, name='user-delete'),
 ]
