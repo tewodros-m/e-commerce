6c71f1ea56c034ab23ae8bc8d7dc56184edbeab9
Ezira Tigab <ezratgab@gmail.com>
2025-01-05 10:42:13 +0300

    clean: code cleaning

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -48,7 +48,7 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
     
     def get_reviews(self, obj):
-        reviews = obj.review_set().all()
+        reviews = obj.review_set.all()
         serializer = ReviewSerializer(reviews, many=True)
         return serializer.data
 
--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -3,18 +3,23 @@ import { Spinner } from 'react-bootstrap';
 
 function Loader() {
   return (
-    <Spinner
-      animation='border'
-      role='status'
-      style={{
-        width: '100px',
-        height: '100px',
-        margin: 'auto',
-        display: 'block',
-      }}
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: "100%", width: "100%" }}
     >
-      <span className='sr-only'>Loading...</span>
-    </Spinner>
+      <Spinner
+        animation="border"
+        role="status"
+        style={{
+          width: "50px",
+          height: "50px",
+          margin: "auto",
+          display: "block",
+        }}
+      >
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    </div>
   );
 }
 
